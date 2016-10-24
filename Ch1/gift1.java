16eca229cb063a1ff290fe50058b32d1c9bb590e
mong <monghim.ng@gmail.com>
2016-10-23 18:16:54 -0700

    task: gift1 finished!

--- Ch1/gift1.java
+++ Ch1/gift1.java
@@ -8,37 +8,55 @@ import java.util.*;
 
 class gift1 {
 
+    public static int getPersonIndex(String[] people, String name){
+        for(int i = 0; i < people.length; i++){
+            if(people[i].equals(name)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
     public static void main (String [] args) throws IOException {
         BufferedReader f = new BufferedReader(new FileReader("gift1.in"));
         PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("gift1.out")));
+
         int np = Integer.parseInt(f.readLine());
-        Map<String, Integer> people = new HashMap<String, Integer>();
+        String[] people = new String[np];
+        int[] bank = new int[np];
+
+        //init
         for(int i = 0; i < np; i++){
-            people.put(f.readLine(), 0);
+            people[i] = f.readLine();
         }
         //each person
         for(int i = 0; i < np; i++){
-            String giver = f.readLine();
+            int giver = getPersonIndex(people, f.readLine());
             StringTokenizer line = new StringTokenizer(f.readLine());
             int money = Integer.parseInt(line.nextToken());
             int receiverNum = Integer.parseInt(line.nextToken());
-            if(receiverNum == 0) {
-                people.put(giver, money);
+
+            if(money == 0) {
+                for(int k = 0; k < receiverNum; k++)
+                    f.readLine();
                 continue;
             }
-            else {
-                people.put(giver, money % receiverNum - money);
-                money = money / receiverNum;
+            else if(receiverNum == 0){
+                bank[giver] += money;
+                continue;
             }
+
+            bank[giver] = bank[giver] - money + money % receiverNum;
+            money = money / receiverNum;
             for(int k = 0; k < receiverNum; k++) {
-                String receiver = f.readLine();
-                people.put(receiver, people.get(receiver) + money);
+                int receiver = getPersonIndex(people, f.readLine());
+                bank[receiver] += money;
             }
         }
 
         //output
-        for(String name: people.keySet()) {
-            out.println(name + " " + people.get(name));
+        for(int i = 0; i < np; i++) {
+            out.println(people[i] + " " + bank[i]);
         }
         out.close();
 
